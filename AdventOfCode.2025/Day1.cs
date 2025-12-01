6631f3e6ba7b6bda0277f6304a56df093a5e1462
Roy Portas <royportas@gmail.com>
2025-12-02 08:39:28 +1000

    WIP

--- AdventOfCode.2025/Day1.cs
+++ AdventOfCode.2025/Day1.cs
@@ -1,64 +1,86 @@
 using AdventOfCode.TestHelpers;
 
-using FluentAssertions;
+using Shouldly;
+
+[assembly: CaptureConsole]
 
 namespace AdventOfCode._2025;
+
+class SafeDial
+{
+    public int Position { get; private set; } = 50;
+    // For Part 1
+    public int LandedOnZeroCount { get; private set; } = 0;
+    // For Part 2
+    public int PassedZeroCount { get; private set; } = 0;
+
+    public void Rotate(string rotation)
+    {
+        char dir = rotation[0];
+        int distance = int.Parse(rotation[1..]);
+
+        if (dir == 'L')
+        {
+            Position -= distance;
+            while (Position < 0)
+            {
+                Position += 100;
+                PassedZeroCount++;
+            }
+        }
+        else
+        {
+            Position += distance;
+            while (Position > 99)
+            {
+                Position -= 100;
+                PassedZeroCount++;
+            }
+        }
+
+        if (Position == 0)
+        {
+            LandedOnZeroCount++;
+            PassedZeroCount++;
+        }
+    }
+}
 
 public class Day1
 {
     int Solve(string[] input, bool part2 = false)
     {
-        int count = 0;
-        int position = 50;
+        SafeDial dial = new();
         foreach (var line in input)
         {
-            var isLeft = line[0] == 'L';
-            var distance = int.Parse(line[1..]);
-            position += isLeft ? -distance : distance;
-
-            while (position > 99)
-            {
-                if (part2)
-                {
-                    count++;
-                }
-                position -= 100;
-            }
-
-            while (position < 0)
-            {
-                if (part2)
-                {
-                    count++;
-                }
-                position += 100;
-            }
-
-            if (position == 0 && !part2)
-            {
-                count++;
-            }
+            dial.Rotate(line);
         }
-        return count;
+        return part2 ? dial.PassedZeroCount : dial.LandedOnZeroCount;
     }
 
     [Fact]
     public async Task Example()
     {
         var input = await TestFile.ReadAllText("./InputFiles/Day1/example.txt");
-        Solve(input).Should().Be(3);
+        Solve(input).ShouldBe(3);
     }
 
     [Fact]
     public async Task ExamplePart2()
     {
-        var input = await TestFile.ReadAllText("./InputFiles/Day1/example.txt");
-        Solve(input, true).Should().Be(6);
+        var input = await TestFile.ReadAllText("./InputFiles/Day1/actual.txt");
+        Solve(input, true).ShouldBe(6);
     }
 
-    [Fact]
-    public async Task TestRotate()
+    [Theory]
+    [InlineData(new[] { "R1000" }, 10)]
+    [InlineData(new[] { "L50", "R50" }, 1)]
+    [InlineData(new[] { "L50", "L50" }, 1)]
+    [InlineData(new[] { "L150", "L50" }, 2)]
+    [InlineData(new[] { "L150", "R50" }, 2)]
+    [InlineData(new[] { "L50", "R101" }, 2)]
+    public async Task TestScenarios(string[] rotations, int expected)
     {
-        Solve(["R1000"], true).Should().Be(10);
+        Solve(rotations, true).ShouldBe(expected);
     }
 }
